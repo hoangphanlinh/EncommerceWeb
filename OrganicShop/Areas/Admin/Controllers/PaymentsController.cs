735caf6f28979cdaf676757485274ec421bfebdf
Linh Ngô <ngothithuylinh2@outlook.com.vn>
2023-07-26 22:51:36 +0700

    CRUD Payment

--- OrganicShop/Areas/Admin/Controllers/PaymentsController.cs
+++ OrganicShop/Areas/Admin/Controllers/PaymentsController.cs
@@ -4,6 +4,7 @@ using OrganicShop.Data.Interfaces;
 using OrganicShop.Data.Services;
 using OrganicShop.Models;
 using X.PagedList;
+using static OrganicShop.Data.ViewModels.PaymentViewModel;
 
 namespace OrganicShop.Areas.Admin.Controllers
 {
@@ -25,6 +26,53 @@ namespace OrganicShop.Areas.Admin.Controllers
             PagedList<Payments> models = new PagedList<Payments>(lspayments, pageNumber, pageSize);
            
             return View(models);
+        }
+        public IActionResult Create()
+        {
+            ViewBag.Allowed = _paymentServices.GetAllowed();
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("PaymentID,Type,Allowed")]Payments model)
+        {
+            if(ModelState.IsValid)
+            {
+                _paymentServices.Create(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+            
+        }
+        public IActionResult Edit(int id) 
+        {
+            ViewBag.Status = _paymentServices.GetAllowed().ToList();
+            var payment = _paymentServices.GetById(id);
+            if(payment == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var viewModel = new UpdatePaymentViewModel()
+                {
+                    PaymentID = payment.PaymentID,
+                    Type = payment.Type,
+                    Allowed = payment.Allowed,
+                };
+                return View(viewModel);
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(UpdatePaymentViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _paymentServices.Update(model);
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ OrganicShop/Data/ViewModels/PaymentViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OrganicShop.Data.ViewModels
+{
+    public class PaymentViewModel
+    {
+        public class UpdatePaymentViewModel
+        {
+            public int PaymentID { get; set; }
+            public string? Type { get; set; }
+            public bool Allowed { get; set; }
+        }
+       
+    }
+}
