a5ac62576c73e419f080e4908155f642e471bde2
Linh Ngô <ngothithuylinh2@outlook.com.vn>
2023-09-13 01:48:52 +0700

    + User: Xử lý trang chủ Xử lý danh mục sản phẩm

---
+++ OrganicShop/Components/ProductDiscountViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrganicShop.Data.Interfaces;
+
+namespace OrganicShop.Components
+{
+    public class ProductDiscountViewComponent : ViewComponent
+    {
+        private readonly IProductServices _productServices;
+        public ProductDiscountViewComponent(IProductServices productServices)
+        {
+            _productServices = productServices;
+        }
+        public IViewComponentResult Invoke() {
+            var result = _productServices.ListProductDiscount().ToList().Take(8);
+            return View(result); 
+        }
+
+    }
+}
--- OrganicShop/Controllers/ShopController.cs
+++ OrganicShop/Controllers/ShopController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OrganicShop.Data.Interfaces;
+using OrganicShop.Models;
+using X.PagedList;
 
 namespace OrganicShop.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductServices _productservices;
+        public ShopController(IProductServices productservices)
         {
-            return View();
+            _productservices = productservices;
+        }
+        public IActionResult Index(int? page, string searchString, int categoryId)
+        {
+            ViewBag.searchString = searchString;
+            ViewBag.categoryId = categoryId;
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 9;
+            var lsproducts = _productservices.GetAllProduct();
+            if (!String.IsNullOrEmpty(searchString) || categoryId != 0)
+            {
+                lsproducts = _productservices.SearchProduct(searchString, categoryId);
+            }
+            PagedList<Products> models = new PagedList<Products>(lsproducts, pageNumber, pageSize);
+            return View(models);
+        }
+        public IActionResult Discount_Product_List(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 9;
+            var lsproducts = _productservices.ProductDiscount();
+           
+            PagedList<Products> models = new PagedList<Products>(lsproducts, pageNumber, pageSize);
+            return View(models);
+        }
+        public IActionResult BestSale_Product_List(int? page)
+        {
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 9;
+            var lsproducts = _productservices.ProductBestSale();
+
+            PagedList<Products> models = new PagedList<Products>(lsproducts, pageNumber, pageSize);
+            return View(models);
         }
     }
 }
