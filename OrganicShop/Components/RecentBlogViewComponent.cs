7188f02fa93a3985aed8c67f5269f26dac83a725
Linh Ngô <ngothithuylinh2@outlook.com.vn>
2023-09-14 02:58:51 +0700

    update blog-details

---
+++ OrganicShop/Components/RecentBlogViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrganicShop.Data.Interfaces;
+
+namespace OrganicShop.Components
+{
+    public class RecentBlogViewComponent : ViewComponent
+    {
+        private readonly IPostServices _postServices;
+        public RecentBlogViewComponent(IPostServices postServices)
+        {
+            _postServices = postServices;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var model = _postServices.ListBlog().TakeLast(3).ToList();
+            return View(model);
+        }
+    }
+}
